add295bcd5e275e06fc9aaa630d7798fc3f47c48
Jared Guevara <jaredpaulgue@berkeley.edu>
2024-10-17 14:39:16 -0700

    Finish implementation of knobs and complete project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('.grid');
 
 function createGrid(dimension) {
+    grid.innerHTML = '';
     for (let i = 0; i < dimension; i++)
         for (let j = 0; j < dimension; j++) {
             const gridSquare = document.createElement('div');
@@ -8,32 +9,46 @@ function createGrid(dimension) {
             gridSquare.style.cssText = `border: 1px solid rgba(0, 0, 0, .025);
                                         background-color: white;
                                         opacity: 1;
-                                        height: ${Math.floor(540 / dimension)}px;
-                                        width: ${Math.floor(540 / dimension)}px;
+                                        height: ${540 / dimension}px;
+                                        width: ${540 / dimension}px;
                                         box-sizing: border-box`;
             grid.appendChild(gridSquare);
-
-            if (i == 0 && j == 0) {
-                gridSquare.style.borderTopLeftRadius = '10px';
-            } else if (i == 0 && j == dimension-1) {
-                gridSquare.style.borderTopRightRadius = '10px'
-            } else if (i == dimension-1 && j == 0) {
-                gridSquare.style.borderBottomLeftRadius = '10px'
-            } else if (i == dimension-1 && j == dimension-1) {
-                gridSquare.style.borderBottomRightRadius = '10px'
-            }
     }
-}
 
-createGrid(16);
+    attachEventListeners();
+}
 
 function darken(ele) {
     ele.style.opacity -= 0.1;
 }
 
-const gridSquares = document.querySelectorAll('.grid-square')
-gridSquares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        darken(square);
+function attachEventListeners() {
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            darken(square);
+        });
     });
+}
+
+function resetGrid() {
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((square) => {
+        square.style.opacity = '1';
+    });
+}
+
+const resetButton = document.querySelector('.reset-grid');
+resetButton.addEventListener('click', () => {
+    resetGrid();
 });
+
+const cycle = [16, 32, 64, 100, 1, 2, 4, 8];
+let clickIndex = 0;
+const changeButton = document.querySelector('.change-grid');
+changeButton.addEventListener('click', () => {
+    clickIndex = (clickIndex + 1) % cycle.length;
+    createGrid(cycle[clickIndex]);
+});
+
+createGrid(16);
