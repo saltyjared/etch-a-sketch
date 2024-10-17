2fb28a56168778b84c91d4622b25aa96af5fc215
Jared Guevara <jaredpaulgue@berkeley.edu>
2024-10-17 13:12:15 -0700

    Finish stylizing of header and content

--- script.js
+++ script.js
@@ -8,34 +8,32 @@ function createGrid(dimension) {
             gridSquare.style.cssText = `border: 1px solid rgba(0, 0, 0, .025);
                                         background-color: white;
                                         opacity: 1;
-                                        height: calc(540px // dimension);
-                                        width: calc(540px // dimension);
-                                        box-sizing: border-box`
+                                        height: ${Math.floor(540 / dimension)}px;
+                                        width: ${Math.floor(540 / dimension)}px;
+                                        box-sizing: border-box`;
             grid.appendChild(gridSquare);
+
+            if (i == 0 && j == 0) {
+                gridSquare.style.borderTopLeftRadius = '10px';
+            } else if (i == 0 && j == dimension-1) {
+                gridSquare.style.borderTopRightRadius = '10px'
+            } else if (i == dimension-1 && j == 0) {
+                gridSquare.style.borderBottomLeftRadius = '10px'
+            } else if (i == dimension-1 && j == dimension-1) {
+                gridSquare.style.borderBottomRightRadius = '10px'
+            }
     }
 }
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridSquare.style.cssText = `border: 1px solid rgba(0, 0, 0, .025);
-                                    background-color: white;
-                                    opacity: 1;
-                                    height: 33px;
-                                    width: 33px;
-                                    box-sizing: border-box`
-        grid.appendChild(gridSquare);
-    }
-}
+createGrid(16);
 
 function darken(ele) {
-    ele.style['opacity'] -= 0.1;
+    ele.style.opacity -= 0.1;
 }
 
 const gridSquares = document.querySelectorAll('.grid-square')
 gridSquares.forEach((square) => {
     square.addEventListener('mouseover', () => {
         darken(square);
-    })
-})
+    });
+});
